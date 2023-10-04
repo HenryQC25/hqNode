f9b116d0e0c723734e2225f47751723a266e1061
HenryQC25 <hquenuncruztic@gmail.com>
2023-10-04 13:45:05 -0600

    agregamos el yargs 3

--- app16.js
+++ app16.js
@@ -1,6 +1,6 @@
 const {crearArchivo} = require('./helpers/multiplicar4')
 const colors = require('colors')
-const argv = require('./config/yargs2')
+const argv = require('./config/yargs3')
 
 console.clear();
 console.log(argv);
